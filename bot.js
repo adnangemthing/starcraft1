7e0a21dafd69b7ac186bdd73adf0869ead7a61d7
adnangemthing <53823301+adnangemthing@users.noreply.github.com>
2019-08-06 17:06:19 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,8 +8,5 @@ client.on('ready', () => {
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
 if (message.content.startsWith(prefix + "bc")) {
 if (!message.member.hasPermission("ADMINISTRATOR"))  return;let args = message.content.split(" ").slice(1);
-  var argresult = args.join(' '); 
-  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
- m.send(`${argresult}\n ${m}`);
-})
+  
   
